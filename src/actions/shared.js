28aaec44f2a0a7f45e8d4c88f0896012e25ef2a7
Eloka Ogbonnia <elokakene@gmail.com>
2023-01-07 19:56:09 +0100

    Update shared.js

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,7 +1,13 @@
 import { getInitialData } from "../utils/api";
+import { receiveTweets} from "../actions/tweets";
+import { receiveUsers} from "../actions/users";
 
 export function handleInitialData () {
     return (dispatch) => {
-        
+        return getInitialData()
+            .then(({ users, tweets}) => {
+                dispatch(receiveUsers(users));
+                dispatch(receiveTweets(tweets));
+            })
     }
 }
